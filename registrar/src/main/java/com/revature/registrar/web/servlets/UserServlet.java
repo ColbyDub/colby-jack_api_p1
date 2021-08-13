c137bdffc1bb1175d53d7cc6559fa3e980293ebc
raneyj <raney.jack99@gmail.com>
2021-08-13 13:39:11 -0500

    simple merging with bookstore

---
+++ registrar/src/main/java/com/revature/registrar/web/servlets/UserServlet.java
@@ -0,0 +1,122 @@
+package com.revature.registrar.web.servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.revature.bookstore.datasource.documents.AppUser;
+import com.revature.bookstore.services.UserService;
+import com.revature.bookstore.util.exceptions.InvalidRequestException;
+import com.revature.bookstore.util.exceptions.ResourceNotFoundException;
+import com.revature.bookstore.util.exceptions.ResourcePersistenceException;
+import com.revature.bookstore.web.dtos.AppUserDTO;
+import com.revature.bookstore.web.dtos.ErrorResponse;
+import com.revature.bookstore.web.dtos.Principal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class UserServlet extends HttpServlet {
+
+    private final Logger logger = LoggerFactory.getLogger(UserServlet.class);
+    private final UserService userService;
+    private final ObjectMapper mapper;
+
+    public UserServlet(UserService userService, ObjectMapper mapper) {
+        this.userService = userService;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getAttribute("filtered"));
+        PrintWriter respWriter = resp.getWriter();
+        resp.setContentType("application/json");
+
+        // Get the session from the request, if it exists (do not create one)
+        HttpSession session = req.getSession(false);
+
+        // If the session is not null, then grab the auth-user attribute from it
+        Principal requestingUser = (session == null) ? null : (Principal) session.getAttribute("auth-user");
+
+        // Check to see if there was a valid auth-user attribute
+        if (requestingUser == null) {
+            String msg = "No session found, please login.";
+            logger.info(msg);
+            resp.setStatus(401);
+            ErrorResponse errResp = new ErrorResponse(401, msg);
+            respWriter.write(mapper.writeValueAsString(errResp));
+            return;
+        } else if (!requestingUser.getUsername().equals("wsingleton")) {
+            String msg = "Unauthorized attempt to access endpoint made by: " + requestingUser.getUsername();
+            logger.info(msg);
+            resp.setStatus(403);
+            ErrorResponse errResp = new ErrorResponse(403, msg);
+            respWriter.write(mapper.writeValueAsString(errResp));
+            return;
+        }
+
+        String userIdParam = req.getParameter("id");
+
+        try {
+
+            if (userIdParam == null) {
+                List<AppUserDTO> users = userService.findAll();
+                respWriter.write(mapper.writeValueAsString(users));
+            } else {
+                AppUserDTO user = userService.findUserById(userIdParam);
+                respWriter.write(mapper.writeValueAsString(user));
+            }
+
+        } catch (ResourceNotFoundException rnfe) {
+            resp.setStatus(404);
+            ErrorResponse errResp = new ErrorResponse(404, rnfe.getMessage());
+            respWriter.write(mapper.writeValueAsString(errResp));
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500); // server's fault
+            ErrorResponse errResp = new ErrorResponse(500, "The server experienced an issue, please try again later.");
+            respWriter.write(mapper.writeValueAsString(errResp));
+        }
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println(req.getAttribute("filtered"));
+        PrintWriter respWriter = resp.getWriter();
+        resp.setContentType("application/json");
+
+        try {
+
+            AppUser newUser = mapper.readValue(req.getInputStream(), AppUser.class);
+            Principal principal = new Principal(userService.register(newUser)); // after this, the newUser should have a new id
+            String payload = mapper.writeValueAsString(principal);
+            respWriter.write(payload);
+            resp.setStatus(201);
+
+        } catch (InvalidRequestException | MismatchedInputException e) {
+            e.printStackTrace();
+            resp.setStatus(400); // client's fault
+            ErrorResponse errResp = new ErrorResponse(400, e.getMessage());
+            respWriter.write(mapper.writeValueAsString(errResp));
+        } catch (ResourcePersistenceException rpe) {
+            resp.setStatus(409);
+            ErrorResponse errResp = new ErrorResponse(409, rpe.getMessage());
+            respWriter.write(mapper.writeValueAsString(errResp));
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500); // server's fault
+        }
+
+
+    }
+}
---
+++ registrar/src/main/java/com/revature/registrar/web/util/ContextLoaderListener.java
@@ -0,0 +1,72 @@
+package com.revature.registrar.web.util;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.client.MongoClient;
+import com.revature.registrar.repository.UserRepository;
+import com.revature.registrar.util.MongoClientFactory;
+import com.revature.registrar.services.UserService;
+import com.revature.registrar.util.PasswordUtils;
+import com.revature.registrar.web.servlets.AuthServlet;
+import com.revature.registrar.web.servlets.HealthCheckServlet;
+import com.revature.registrar.web.servlets.UserServlet;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.File;
+
+public class ContextLoaderListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        System.out.println("It's aliiiiive!!!");
+
+        //MongoClient mongoClient = MongoClientFactory.getInstance().getConnection();
+        PasswordUtils passwordUtils = new PasswordUtils();
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+
+        UserRepository userRepo = new UserRepository();
+        UserService userService = new UserService(userRepo, passwordUtils);
+
+        HealthCheckServlet healthCheckServlet = new HealthCheckServlet();
+        UserServlet userServlet = new UserServlet(userService, mapper);
+        AuthServlet authServlet = new AuthServlet(userService, mapper);
+
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.addServlet("UserServlet", userServlet).addMapping("/users/*");
+        servletContext.addServlet("AuthServlet", authServlet).addMapping("/auth");
+        servletContext.addServlet("HealthCheckServlet", healthCheckServlet).addMapping("/health");
+
+        configureLogback(servletContext);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("Goodbye, cruel world!!!");
+        MongoClientFactory.getInstance().cleanUp();
+    }
+
+    private void configureLogback(ServletContext servletContext) {
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        JoranConfigurator logbackConfig = new JoranConfigurator();
+        logbackConfig.setContext(loggerContext);
+        loggerContext.reset();
+
+        String logbackConfigFilePath = servletContext.getRealPath("") + File.separator + servletContext.getInitParameter("logback-config");
+
+        try {
+            logbackConfig.doConfigure(logbackConfigFilePath);
+        } catch (JoranException e) {
+            e.printStackTrace();
+            System.out.println("An unexpected exception occurred. Unable to configure Logback.");
+        }
+
+    }
+
+}
